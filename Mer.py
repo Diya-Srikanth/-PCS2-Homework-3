fb2a178103577b62e78d35af511a79fea121c589
Diya-Srikanth <72697147+Diya-Srikanth@users.noreply.github.com>
2020-11-10 17:48:15 +0100

    Add files via upload

---
+++ Mer.py
@@ -0,0 +1,11 @@
+with open('yo.txt', 'r') as file:
+    content = file.read()
+    content = content.strip(' ').split('\n')
+    l=[]
+    for i in range(len(content)):
+        if i%2 !=0:
+            a=list(map(int,content[i].split()))
+            for x in a:
+                l.append(x)
+                l = sorted(l)
+    print(' '.join([str(x) for x in l]))
