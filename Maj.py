d3d5855733cf72e8c4b98241b002b2370c8ba321
Diya-Srikanth <72697147+Diya-Srikanth@users.noreply.github.com>
2020-11-17 17:56:36 +0100

    Update Maj.py

--- Maj.py
+++ Maj.py
@@ -1,10 +1,10 @@
 with open("rosalind_maj.txt", "r") as file:
     k, n = map(int, file.readline().strip().split())
-    A = [line.strip().split() for line in file]
+    a = [line.strip().split() for line in file]
 
 for i in range(k):
-    c = [A[i].count(A[i][j]) > n/2 for j in range(n)]
+    c = [a[i].count(a[i][j]) > n/2 for j in range(n)]
     if any(c):
-        print(A[i][c.index(True)], end=" ")
+        print(a[i][c.index(True)], end=" ")
     else:
         print("-1", end=" ")
