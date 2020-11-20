c5358fa89e224d929ce2db0364a211fece95a3da
Diya-Srikanth <72697147+Diya-Srikanth@users.noreply.github.com>
2020-11-20 19:35:56 +0100

    Update Inv.py

--- Inv.py
+++ Inv.py
@@ -1,33 +1,31 @@
-def SortCount(A):
-   l = len(A)
-   if l > 1:
-      n = l//2
-      C = A[:n]
-      D = A[n:]
-      C, c = SortCount(A[:n])
-      D, d = SortCount(A[n:])
-      B, b = MergeCount(C,D)
-      return B, b+c+d
-   else:
-      return A, 0
+def inv(arr):
+    inversions = 0
+    if len(arr) == 1:
+        return arr, 0
+    else:
+        a = arr[:len(arr)//2]
+        b = arr[len(arr)//2:]
+        a, ai = inv(a)
+        b, bi = inv(b)
+        c = []
+        i = 0
+        j = 0
+        inversions = ai + bi
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+            inversions += (len(a)-i)
+    c += a[i:]
+    c += b[j:]
+    return c, inversions
 
-def MergeCount(A,B):
-   count = 0
-   M = []
-   while A and B:
-      if A[0] <= B[0]:
-         M.append(A.pop(0))
-      else:
-         count += len(A)
-         M.append(B.pop(0))
-   M  += A + B
-   return M, count
-
-with open('rosalind_inv.txt') as f:
-    i = 0
-    for line in f:
-        i += 1
-        if i == 2:
-            numbers = [int(x) for x in line.strip().split(' ')]
-
-print(SortCount(numbers)[1])
+if __name__ == "__main__":
+    with open("rosalind_inv.txt", "r") as f:
+        n = int(f.readline().strip())
+        A = [int(i) for i in f.readline().strip().split()]
+    arr, inv_count = inv(A)
+    print(inv_count)
